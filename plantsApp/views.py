878bc3dde923a07cd2c552ee1d30a7185734fb2a
JosephRidge <josephridge18@gmail.com>
2026-04-20 13:21:34 +0300

    update

--- plantsApp/views.py
+++ plantsApp/views.py
@@ -3,8 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello Deepseek LAB!! \n\nhttp://127.0.0.1:8000/")
-
+    context = {}
+    return render(request, "base.html", context)
 
 def about(request):
     return HttpResponse("Welcome to Deepseeks about page!! \n\nhttp://127.0.0.1:8000/about/")
+ 
